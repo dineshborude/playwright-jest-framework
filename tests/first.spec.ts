988d65e0e007d04f05fa995ec38fbbde9fdf72b8
Dinesh Borude <borudedinesh2@gmail.com>
2025-07-12 23:59:07 +0530

    Setup

---
+++ tests/first.spec.ts
@@ -0,0 +1,15 @@
+import {test} from '@playwright/test';
+
+test("First Test Block", () => {
+
+    console.log("First Test");
+
+})
+
+
+test("Second Test Block", () => {
+
+    console.log("First Test");
+    
+
+})
